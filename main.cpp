8a29eb2cde4253e9b4a62bf3d10f94e44aede9bf
mellw0101 <melwivensson@gmail.com>
2024-01-28 12:59:16 -0800

    Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,32 @@
 
 template <typename T>
 using vec = std::vector<T>;
-using string = std::string;
+using string = std::string; 
 
-int pacman(vec<string> & packages) {
+int pacman(vec<string> packages) {
     string packages_string = "sudo pacman -S --noconfirm ";
     for (const string & part : packages) {
         packages_string += part + " ";
     }
     packages_string.erase(packages_string.length() - 1);
-    std::system(packages_string.c_str());
+    int status = std::system(packages_string.c_str());
+    if (status != 0) {
+        std::cerr << "system call failed" << std::endl;
+    }
+    return status;
 }
-
+int pacstrap(vec<string> packages) {
+    string packages_string = "pacstrap -i /mnt ";
+    for (const string & part : packages) {
+        packages_string += part + " ";
+    }
+    packages_string.erase(packages_string.length() - 1);
+    int status = std::system(packages_string.c_str());
+    if (status != 0) {
+        std::cerr << "system call failed" << std::endl;
+    }
+    return status;
+}
 int main() {
-
+    pacstrap({"base", "linux", "linux-firmware", "sudo", "nano"});
 }
